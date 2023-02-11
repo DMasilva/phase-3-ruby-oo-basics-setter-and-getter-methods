2ff5f32bbf2b3a2bab14c625332585f9eeed94f3
David <david.owuor@student.moringaschool.com>
2023-02-11 13:54:28 +0300

    complet

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    #name setter
+    def name= (name)
+        @name = name
+    end
 
+    #getter name
+    def name
+        @name
+    end
+
+    #breed setter
+    def breed= (breed)
+        @breed = breed
+    end
+
+    #getter breed
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    #name setter
+    def name= (name)
+        @name = name
+    end
 
+    #getter name
+    def name
+        @name
+    end
+
+    #job setter
+    def job= (job)
+        @job = job
+    end
+
+    #getter job
+    def job
+        @job
+    end
 end
